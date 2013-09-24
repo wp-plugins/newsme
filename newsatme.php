340a1d62714b05345bb9f65e0fd27bdd159c73b5
newsatme <newsatme@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-24 15:40:36 +0000

    Version 1.0.1 - typo fixed
    git-svn-id: https://plugins.svn.wordpress.org/newsme/trunk@777805 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- newsatme.php
+++ newsatme.php
@@ -3,7 +3,7 @@
 Plugin Name: wpNewsAtMe
 Description: wpNewsAtMe sends email alerts using news@me
 Author: News@me
-Version: 1.0.0
+Version: 1.0.1
 Text Domain: wpnewsatme
  */
 /*  Copyright 2013  News@me 
@@ -33,7 +33,7 @@ function wpnewsatme_init() {
 
 class wpNewsAtMe {
 
-  const VERSION = '1.0.0'; 
+  const VERSION = '1.0.1'; 
   const WPDOMAIN = 'wpnewsatme';
   const DEBUG = false;
   const TAGS_META_KEY = '_newsatme_tags'; 
