3d960f84569500057ee9f3181a3dc0028af1c5ea
newsatme <newsatme@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-13 10:19:59 +0000

    Hotfix on version 2.1.6
    git-svn-id: https://plugins.svn.wordpress.org/newsme/trunk@874623 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/views/get_key_page.html.php
@@ -0,0 +1,18 @@
+<div class="wrap">
+  <?php screen_icon(); ?>
+  <h2 class="newsatme-header"><?php _e('News@me', wpNewsAtMe::WPDOMAIN); ?></h2>
+  <ul class="global-nav">
+    <li><a href="https://newsatme.com/en/how-it-works/?utm_source=wordpress-plugin&utm_medium=link-how-it-works&utm_campaign=wordpress-plugin" target="_blank">How it works</a></li>
+    <li><a href="https://app.newsatme.com/users/sign_up?utm_source=wordpress-plugin&utm_medium=link-get-started&utm_campaign=wordpress-plugin" target="_blank">Get started</a></li>
+    <li><a href="http://help.newsatme.com/?utm_source=wordpress-plugin&utm_medium=link-support&utm_campaign=wordpress-plugin" target="_blank">Support</a></li>
+    <li><a href="https://newsatme.com/about-us/?utm_source=wordpress-plugin&utm_medium=link-about-us&utm_campaign=wordpress-plugin" target="_blank">About us</a></li>
+    <li><a href="https://app.newsatme.com/users/sign_in?utm_source=wordpress-plugin&utm_medium=link-log-in&utm_campaign=wordpress-plugin" target="_blank">Log in</a></li>
+  </ul>
+	<p>News@me converts your occasional visitors into regular readers and sends them articles of your site based on their interests. 
+	<br>
+	To use News@me you may need to sign up for an API key. Get started with the button below.</p>
+    <a href="https://app.newsatme.com/users/sign_up?utm_source=wordpress-plugin&utm_medium=button-create-your-api-key&utm_campaign=wordpress-plugin" class="button button-primary" target="_blank">Create your News@me API key</a>
+	<br>
+  <p><a href="<?php echo $link; ?>">I already have a key</a></p>
+  
+</div>
